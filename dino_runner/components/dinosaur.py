a01aebc30deee78020a288398ca2d9b0aeed9ced
Omar-ztc <omar.tapia@fundacion-jala.org>
2022-11-01 15:20:06 -0400

    tarea 1
    implementacion del metodo de agacharse para el dinosaurio

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -7,6 +7,7 @@ class Dinosaur(Sprite):
     Y_pos = 310
     JUMP_VELOCITY= 8.5
 
+
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -15,23 +16,37 @@ class Dinosaur(Sprite):
         self.step_index = 0
         self.dino_run = True
         self.dino_jump = False
-
+        self.dino_duck = False
         self.jump_velocity = self.JUMP_VELOCITY
+
     
     def update(self, user_input):
         if self.dino_run:
             self.run()
+
         elif self.dino_jump:
             self.jump()
-        elif self.dino_duck:
+
+        else:
             self.duck()
         
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump= True
             self.dino_run = False
 
+
         elif not self.dino_jump:
             self.dino_jump = False
+            self.dino_run = True
+        
+        if user_input[pygame.K_DOWN]:
+            self.dino_duck = True
+            self.dino_run = False
+
+        
+        
+        elif not self.dino_duck:
+            self.dino_duck = False
             self.dino_run = True
 
 
@@ -56,12 +71,15 @@ class Dinosaur(Sprite):
             self.dino_rect.y = self.Y_pos
             self.jump_velocity = self.JUMP_VELOCITY
     
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_pos
+        self.dino_rect.y = 350
+        self.step_index += 1 
+
+
 
         
-        
-
-
-
-
     def draw(self, screen):
         screen.blit(self.image,(self.dino_rect.x,self.dino_rect.y))
